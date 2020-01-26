d755fc4dfd0fcd4cdff13c085f9575005daa1957
doroddong <dongpang1014@ajou.ac.kr>
2020-01-26 19:10:10 +0900

    Add print_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title,:description)
+      params.require(:post).permit(:title,:description,:user_id)
     end
   private
   def load_post
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_083524) do
+ActiveRecord::Schema.define(version: 2020_01_26_094143) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2020_01_26_083524) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
