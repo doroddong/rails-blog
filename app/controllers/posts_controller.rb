49dd18035dcdaeec9e202edae0f92a36ab3910c7
doroddong <dongpang1014@ajou.ac.kr>
2020-01-26 14:42:05 +0900

    Add index_page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+    
+  def index
+    @post = Post.all
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+  def edit
+
+  end
+  def show
+
+  end
+  def update
+
+  end
+  def destroy
+
+  end
 end
