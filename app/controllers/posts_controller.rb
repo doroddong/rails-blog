0526342f16e836cc5e1a16339e633d148373e0ec
doroddong <dongpang1014@ajou.ac.kr>
2020-01-27 03:16:29 +0900

    Customize

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :load_post, only: [:show,:edit,:update,:destroy]
-
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @post = Post.all
